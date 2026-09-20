0b3addfbb52c02826de1ee33dfb4172bb61a0156
MsCodin <mihaelastaneva06@gmail.com>
2026-09-20 13:52:56 -0400

    Add Flask server and update unit tests

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,22 +1,46 @@
-import unittest
-from EmotionDetection.emotion_detection import emotion_detector
+import requests
+import json
 
-class TestEmotionDetection(unittest.TestCase):
-    def test_emotion_detector(self):
-        result_1 = emotion_detector("I am glad this happened")
-        self.assertEqual(result_1['dominant_emotion'], 'joy')
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
 
-        result_2 = emotion_detector("I am really mad about this")
-        self.assertEqual(result_2['dominant_emotion'], 'anger')
+    response = requests.post(url, json=myobj, headers=headers)
 
-        result_3 = emotion_detector("I feel disgusted just hearing about this")
-        self.assertEqual(result_3['dominant_emotion'], 'disgust')
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
-        result_4 = emotion_detector("I am so sad about this")
-        self.assertEqual(result_4['dominant_emotion'], 'sadness')
+    response_dict = json.loads(response.text)
+    emotions = response_dict['emotionPredictions'][0]['emotion']
 
-        result_5 = emotion_detector("I am really afraid that this will happen")
-        self.assertEqual(result_5['dominant_emotion'], 'fear')
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
 
-if __name__ == '__main__':
-    unittest.main()
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
